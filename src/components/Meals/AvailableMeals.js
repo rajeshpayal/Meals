c28791a8a6668820efd06fab1da90db595759cf3
rajeshpayal <rajeshpayal1094@gmail.com>
2023-05-23 15:10:22 +0530

    fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,23 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AvailableMeals.module.css";
-import { DUMMY_MEALS } from "./Dummy_meals";
+
 import Card from "../UI/Card";
 import MealItem from "./MealItem";
+import axios from "axios";
+import { url } from "../../constant/constant";
 const AvailableMeals = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({ isError: false, errorMsg: "" });
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        const { data } = response;
+        const loadedMeals = [];
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            price: data[key].price,
+            description: data[key].description,
+          });
+          setData(loadedMeals);
+        }
+      })
+      .catch((err) => setError({ isError: true, errorMsg: err.message }))
+      .finally(() => setLoading(false));
+  }, []);
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>
-          {DUMMY_MEALS.map((meal, idx) => (
-            <MealItem
-              key={idx}
-              name={meal.name}
-              id={meal.id}
-              description={meal.description}
-              price={meal.price}
-            />
-          ))}
-        </ul>
+        {loading && <p>Loading...</p>}
+        {!loading && (
+          <ul>
+            {data.map((meal, idx) => (
+              <MealItem
+                key={idx}
+                name={meal.name}
+                id={meal.id}
+                description={meal.description}
+                price={meal.price}
+              />
+            ))}
+          </ul>
+        )}
       </Card>
     </section>
   );
---
+++ src/constant/constant.js
@@ -0,0 +1,2 @@
+export const url =
+  "https://postdatamovie-default-rtdb.firebaseio.com/meals.json";
